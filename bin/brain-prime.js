863983f2c0b82b56e03c04f31498dfc937398cf3
Anton Liubimov <ant.liubimov@gmail.com>
2022-08-15 22:30:41 +0500

    started prime play

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import primePlay from '../src/prime-play.js';
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+primePlay(rules);
---
+++ src/prime-play.js
@@ -0,0 +1,7 @@
+import { getRandomNumber, play } from './index.js';
+
+
+
+const primePlay = (rules) => play(rules, primePlayQuestion);
+
+export default primePlay;
