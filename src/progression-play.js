bed0b3aa9e46155d0cd0f5fccbe8aceedfd3fb3b
Anton Liubimov <ant.liubimov@gmail.com>
2022-08-15 20:46:58 +0500

    done brain-progression

--- src/progression-play.js
+++ src/progression-play.js
@@ -1,14 +1,12 @@
 import { getRandomNumber, play } from './index.js';
 
+const progressionLength = 10;
+
 const getProgression = () => {
   const progression = [];
-  const minLength = 5;
-  const maxLength = 10;
-  const progressionLength = getRandomNumber(minLength, maxLength);
   const minStep = 1;
   const maxStep = 10;
   const progressionStep = getRandomNumber(minStep, maxStep);
-
   const minStart = 1;
   const maxStart = 100;
   let progressionStart = getRandomNumber(minStart, maxStart);
@@ -19,7 +17,22 @@ const getProgression = () => {
   return progression;
 };
 
-const progressionPlayQuestion = () => {};
+const progressionPlayQuestion = () => {
+  const result = {};
+  const hiddenIndex = getRandomNumber(0, progressionLength - 1);
+  const progression = getProgression();
+  const tmpArr = [];
+  for (let i = 0; i < progressionLength; i += 1) {
+    if (i !== hiddenIndex) {
+      tmpArr.push(progression[i]);
+    } else {
+      tmpArr.push('..');
+    }
+  }
+  result.str = tmpArr.join(' ');
+  result.answer = progression[hiddenIndex].toString();
+  return result;
+};
 
 const progressionPlay = (rules) => play(rules, progressionPlayQuestion);
 
